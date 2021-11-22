9775929571d8b4e452546f4b5bf889b92490e31a
fdocharles <fdocharles@gmail.com>
2021-11-22 14:21:29 -0500

    completed contact page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var aboutmeRouter = require("./routes/aboutme");
 var projectsRouter = require("./routes/projects");
+var contactmeRouter = require("./routes/contactme");
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/aboutme", aboutmeRouter);
 app.use("/projects", projectsRouter);
+app.use("/contact", contactmeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("projects");
+  res.render("projects", [
+    {
+      title: "",
+      subTitle: "",
+      description: "",
+      imagePath: "",
+    },
+    {
+      title: "",
+      subTitle: "",
+      description: "",
+      imagePath: "",
+    },
+    {
+      title: "",
+      subTitle: "",
+      description: "",
+      imagePath: "",
+    },
+    {
+      title: "",
+      subTitle: "",
+      description: "",
+      imagePath: "",
+    },
+  ]);
 });
 
 module.exports = router;
